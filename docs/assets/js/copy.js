ba59b3358f2a294c356ad1ac6d8d3e5ae4b1fb4d
John  Walshe <jwalshedev@gmail.com>
2025-02-07 22:43:04 +0000

    Update copy.js

--- docs/assets/js/copy.js
+++ docs/assets/js/copy.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".expand-button").forEach(button => {
         button.addEventListener("click", function () {
             let codeBlock = button.closest(".code-block");
-            let codeContainer = codeBlock ? codeBlock.querySelector(".code-container") : null;
 
             if (!codeContainer) {
                 console.error("Error: .code-container not found inside .code-block");
@@ -13,12 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (isExpanded) {
                 // Expand
-                codeContainer.style.maxHeight = codeContainer.scrollHeight + "px";
                 button.querySelector(".unfold-icon").classList.add("hidden");
                 button.querySelector(".fold-icon").classList.remove("hidden");
             } else {
                 // Collapse
-                codeContainer.style.maxHeight = null;
                 button.querySelector(".unfold-icon").classList.remove("hidden");
                 button.querySelector(".fold-icon").classList.add("hidden");
             }
